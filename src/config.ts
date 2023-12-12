1e92e45d5345b0c4e9f8da5118094e56a45e7562
Raluca Nicola <rnicola@esri.com>
2023-12-12 10:31:42 +0100

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const mapConfig = {
+  'web-map-id': '99f098808fc048bb89f89695106800aa'
+};
+
+export const portalUrl = 'https://geoxc.maps.arcgis.com';
+
+export const applicationTitle = 'ArcGIS Web Template';
+
+export const displayInfoModalDefault = false;
